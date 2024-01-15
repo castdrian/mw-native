314e739af50858f467c7726f0cb01ac0c5a26783
castdrian <adrifcastr@gmail.com>
2024-01-15 08:43:44 +0100

    fix: restore the expo router stuff that the autogenerated stuff undid

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+	plugins: [
+		// Required for expo-router
+		'expo-router/babel',
+	  ],
   };
 };
