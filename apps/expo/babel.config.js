a270c94e0312d3725452ca6e33d405e173c8ead3
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-10 11:21:14 +0100

    feat: use quick crypto

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -6,6 +6,18 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          alias: {
+            crypto: "react-native-quick-crypto",
+            stream: "stream-browserify",
+            buffer: "@craftzdog/react-native-buffer",
+          },
+        },
+      ],
+    ],
   };
 };
