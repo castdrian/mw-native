a2b70eee3a8506ee12b205a7df3da92fa7bc4023
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-14 11:46:14 +0100

    fix: control touch events on iOS

--- apps/expo/src/components/player/ControlsOverlay.tsx
+++ apps/expo/src/components/player/ControlsOverlay.tsx
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { TouchableWithoutFeedback, View } from "react-native";
 
 import type { HeaderData } from "./Header";
+import { usePlayerStore } from "~/stores/player/store";
 import { BottomControls } from "./BottomControls";
 import { Header } from "./Header";
 import { MiddleControls } from "./MiddleControls";
@@ -10,11 +11,19 @@ interface ControlsOverlayProps {
 }
 
 export const ControlsOverlay = ({ headerData }: ControlsOverlayProps) => {
+  const idle = usePlayerStore((state) => state.interface.isIdle);
+  const setIsIdle = usePlayerStore((state) => state.setIsIdle);
+
+  const handleTouch = () => {
+    setIsIdle(!idle);
+  };
   return (
-    <View className="absolute left-0 top-0 flex h-full w-full flex-1">
-      <Header data={headerData} />
-      <MiddleControls />
-      <BottomControls />
-    </View>
+    <TouchableWithoutFeedback onPress={handleTouch}>
+      <View className="absolute left-0 top-0 flex h-full w-full flex-1">
+        <Header data={headerData} />
+        <MiddleControls />
+        <BottomControls />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
