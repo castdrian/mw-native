439ba8c7e5301b9324c18e2f3e9b68afd2ffad3d
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-14 21:53:33 +0100

    fix: fix the controls so they don't intefere with bottom controls

--- apps/expo/src/components/player/ControlsOverlay.tsx
+++ apps/expo/src/components/player/ControlsOverlay.tsx
@@ -1,7 +1,6 @@
-import { TouchableWithoutFeedback, View } from "react-native";
+import { View } from "react-native";
 
 import type { HeaderData } from "./Header";
-import { usePlayerStore } from "~/stores/player/store";
 import { BottomControls } from "./BottomControls";
 import { Header } from "./Header";
 import { MiddleControls } from "./MiddleControls";
@@ -11,19 +10,11 @@ interface ControlsOverlayProps {
 }
 
 export const ControlsOverlay = ({ headerData }: ControlsOverlayProps) => {
-  const idle = usePlayerStore((state) => state.interface.isIdle);
-  const setIsIdle = usePlayerStore((state) => state.setIsIdle);
-
-  const handleTouch = () => {
-    setIsIdle(!idle);
-  };
   return (
-    <TouchableWithoutFeedback onPress={handleTouch}>
-      <View className="absolute left-0 top-0 flex h-full w-full flex-1">
-        <Header data={headerData} />
-        <MiddleControls />
-        <BottomControls />
-      </View>
-    </TouchableWithoutFeedback>
+    <View className="absolute left-0 top-0 flex h-full w-full flex-1">
+      <Header data={headerData} />
+      <MiddleControls />
+      <BottomControls />
+    </View>
   );
 };
--- apps/expo/src/components/player/MiddleControls.tsx
+++ apps/expo/src/components/player/MiddleControls.tsx
@@ -1,22 +1,32 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 
+import { usePlayerStore } from "~/stores/player/store";
 import { Controls } from "./Controls";
 import { PlayButton } from "./PlayButton";
 import { SeekButton } from "./SeekButton";
 
 export const MiddleControls = () => {
+  const idle = usePlayerStore((state) => state.interface.isIdle);
+  const setIsIdle = usePlayerStore((state) => state.setIsIdle);
+
+  const handleTouch = () => {
+    setIsIdle(!idle);
+  };
+
   return (
-    <View style={styles.container}>
-      <Controls className="mr-24">
-        <SeekButton type="backward" />
-      </Controls>
-      <Controls>
-        <PlayButton />
-      </Controls>
-      <Controls>
-        <SeekButton type="forward" />
-      </Controls>
-    </View>
+    <TouchableWithoutFeedback onPress={handleTouch}>
+      <View style={styles.container}>
+        <Controls className="mr-24">
+          <SeekButton type="backward" />
+        </Controls>
+        <Controls>
+          <PlayButton />
+        </Controls>
+        <Controls>
+          <SeekButton type="forward" />
+        </Controls>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
