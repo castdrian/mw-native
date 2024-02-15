149daa3435e6f19c9328a2b07d06cdbfab7acb93
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-15 20:59:54 +0100

    feat: pretty native context menu on search items

--- apps/expo/src/components/item/item.tsx
+++ apps/expo/src/components/item/item.tsx
@@ -1,4 +1,5 @@
 import { Image, Keyboard, TouchableOpacity, View } from "react-native";
+import ContextMenu from "react-native-context-menu-view";
 import { useRouter } from "expo-router";
 
 import { Text } from "~/components/ui/Text";
@@ -23,28 +24,32 @@ export default function Item({ data }: { data: ItemData }) {
     });
   };
 
+  const contextMenuActions = [
+    { title: "Bookmark" },
+    ...(type === "movie" ? [{ title: "Download" }] : []),
+  ];
+
+  const onContextMenuPress = (_e: unknown) => {
+    // do stuff
+  };
+
   return (
     <TouchableOpacity onPress={handlePress} style={{ width: "100%" }}>
-      {
-        <View className="w-full">
+      <View className="w-full">
+        <ContextMenu actions={contextMenuActions} onPress={onContextMenuPress}>
           <View className="mb-2 aspect-[9/14] w-full overflow-hidden rounded-2xl">
-            <Image
-              source={{
-                uri: posterUrl,
-              }}
-              className="h-full w-full"
-            />
+            <Image source={{ uri: posterUrl }} className="h-full w-full" />
           </View>
-          <Text className="font-bold">{title}</Text>
-          <View className="flex-row items-center gap-3">
-            <Text className="text-xs text-gray-600">
-              {type === "tv" ? "Show" : "Movie"}
-            </Text>
-            <View className="h-1 w-1 rounded-3xl bg-gray-600" />
-            <Text className="text-sm text-gray-600">{year}</Text>
-          </View>
+        </ContextMenu>
+        <Text className="font-bold">{title}</Text>
+        <View className="flex-row items-center gap-3">
+          <Text className="text-xs text-gray-600">
+            {type === "tv" ? "Show" : "Movie"}
+          </Text>
+          <View className="h-1 w-1 rounded-3xl bg-gray-600" />
+          <Text className="text-sm text-gray-600">{year}</Text>
         </View>
-      }
+      </View>
     </TouchableOpacity>
   );
 }
