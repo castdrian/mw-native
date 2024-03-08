70f32abdf8bcf962ead5a2663d33ba2c1a4f9c50
Jorrin <jorrinkievit@hotmail.com>
2024-03-09 00:38:53 +0100

    fix ci

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -11,7 +11,7 @@ const config = {
     "prettier-plugin-tailwindcss",
   ],
   tailwindConfig: fileURLToPath(
-    new URL("../../tooling/tailwind/web.ts", import.meta.url),
+    new URL("../../tooling/tailwind/native.ts", import.meta.url),
   ),
   tailwindFunctions: ["cn", "cva"],
   importOrder: [
